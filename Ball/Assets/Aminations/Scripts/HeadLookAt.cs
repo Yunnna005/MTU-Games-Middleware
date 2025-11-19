11099fd97fd774f2f59d5a316777bd8d2f946e0d
Student Anna Kovalenko (TL_KCOMP_B_Y4) <t00243622@mymtu.ie>
2025-11-19 10:31:59 +0000

    Add limits to lookAt
    Signed-off-by: Student Anna Kovalenko (TL_KCOMP_B_Y4) <t00243622@mymtu.ie>

--- Ball/Assets/Aminations/Scripts/HeadLookAt.cs
+++ Ball/Assets/Aminations/Scripts/HeadLookAt.cs
@@ -3,12 +3,27 @@ using UnityEngine;
 public class HeadLookAt : MonoBehaviour
 {
     public Transform target;
+    public float maxYaw = 55f;   
+    public float maxPitch = 30f; 
 
     private void LateUpdate()
     {
         if (target == null) return;
 
         Vector3 dir = target.position - transform.position;
-        transform.LookAt(transform.position + dir);
+        Quaternion targetRot = Quaternion.LookRotation(dir);
+
+        Quaternion localRot = Quaternion.Inverse(transform.parent.rotation) * targetRot;
+        Vector3 euler = localRot.eulerAngles;
+
+
+        euler.x = (euler.x > 180) ? euler.x - 360 : euler.x;
+        euler.y = (euler.y > 180) ? euler.y - 360 : euler.y;
+        euler.z = 0f;
+
+        euler.x = Mathf.Clamp(euler.x, -maxPitch, maxPitch);
+        euler.y = Mathf.Clamp(euler.y, -maxYaw, maxYaw);
+
+        transform.localRotation = Quaternion.Euler(euler);
     }
 }
