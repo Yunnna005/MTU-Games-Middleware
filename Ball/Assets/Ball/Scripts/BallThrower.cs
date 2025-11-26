f82f5c9b9c1bad64ed8b428f4f524a87632a58ed
Student Anna Kovalenko (TL_KCOMP_B_Y4) <t00243622@mymtu.ie>
2025-11-26 09:19:51 +0000

    Add random values for spawning the spheres
    Signed-off-by: Student Anna Kovalenko (TL_KCOMP_B_Y4) <t00243622@mymtu.ie>

--- Ball/Assets/Ball/Scripts/BallThrower.cs
+++ Ball/Assets/Ball/Scripts/BallThrower.cs
@@ -8,6 +8,11 @@ public class BallThrower : MonoBehaviour
     public float throwForce = 10f;
     public float distance = 1f;
 
+    public float minScale = 0.2f;
+    public float maxScale = 1f;
+    public float minMass = 0.2f;
+    public float maxMass = 2f;
+
     public void Throw(InputAction.CallbackContext context)
     {
         if (context.performed)
@@ -17,6 +22,13 @@ public class BallThrower : MonoBehaviour
                 cameraTransform.position + cameraTransform.forward * distance,
                 Quaternion.identity
             );
+
+            float scaleValue = Random.Range(minScale, maxScale);
+            float massValue = Random.Range(minMass, maxMass);
+
+            newSphere.transform.localScale = Vector3.one * scaleValue;
+            newSphere.mass = massValue;
+
             newSphere.overrideAfterCollision(newSphere.transform.position, Vector3.zero);
             newSphere.velocity = cameraTransform.forward * throwForce;
         }
