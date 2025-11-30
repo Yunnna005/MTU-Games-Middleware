496ab47a6f7adc8acd7c47d8918644dccd392819
Yunnna005 <annakovalenko0500@gmail.com>
2025-11-30 21:13:23 +0000

    Basic Dungeon is done
    Signed-off-by: Yunnna005 <annakovalenko0500@gmail.com>

--- Ball/Assets/Project 2/Scripts/DungeonGeneration.cs
+++ Ball/Assets/Project 2/Scripts/DungeonGeneration.cs
@@ -13,6 +13,7 @@ public class DungeonGeneration : MonoBehaviour
     public Vector2 size;
     public int startPos = 0;
     public GameObject room;
+    public Vector2 offset;
 
     List<Cell> board;
 
@@ -28,19 +29,30 @@ public class DungeonGeneration : MonoBehaviour
 
     void GenerateDungeon()
     {
+        for (int i = 0; i < size.x; i++)
+        {
+            for (int j = 0; j < size.y; j++)
+            {
 
+                Cell currentCell = board[Mathf.FloorToInt(i + j * size.x)];
+                if (currentCell.visited)
+                {
+                    var newRoom = Instantiate(room, new Vector3(i * offset.x, 0, -j * offset.y), Quaternion.identity, transform).GetComponent<RoomBahaviour>();
+                    newRoom.RoomUpdate(board[Mathf.FloorToInt(i + j * size.x)].status);
+
+                    newRoom.name += " " + i + "-" + j;
+                }
+            }
+        }
     }
 
-    void MazeGenerator()
+    public void MazeGenerator()
     {
         board = new List<Cell>();
 
         for (int i = 0; i < size.x * size.y; i++)
         {
-            for (int j = 0; j < size.y; j++)
-            {
-                board.Add(new Cell());
-            }
+            board.Add(new Cell());
         }
 
         int currentCell = startPos;
@@ -54,6 +66,11 @@ public class DungeonGeneration : MonoBehaviour
             k++;
 
             board[currentCell].visited = true;
+
+            if (currentCell == board.Count -1 )
+            {
+                break;
+            }
 
             // Check neighbors
             List<int> neighbors = CheckNeighbors(currentCell);
@@ -117,13 +134,13 @@ public class DungeonGeneration : MonoBehaviour
         List<int> neighbors = new List<int>();
 
         //check up
-        if (cell - size.x >= 0 && board[Mathf.FloorToInt(cell - size.x)].visited)
+        if (cell - size.x >= 0 && !board[Mathf.FloorToInt(cell - size.x)].visited)
         {
             neighbors.Add(Mathf.FloorToInt(cell - size.x));
         }
 
         //check down
-        if (cell + size.x < board.Count && board[Mathf.FloorToInt(cell + size.x)].visited)
+        if (cell + size.x < board.Count && !board[Mathf.FloorToInt(cell + size.x)].visited)
         {
             neighbors.Add(Mathf.FloorToInt(cell + size.x));
         }
